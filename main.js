7b1972db71a901087a3d4325af57c9fb3b1d48c0
ebdevStudent <78286626+ebdevStudent@users.noreply.github.com>
2021-03-18 21:34:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function changeimg(){
+    document.getElementById("img1").src="tennispiccodinghomework.png"
+    }  
+function redborder(){
+    document.getElementById("img3").style.borderStyle="solid";
+    document.getElementById("img3").style.borderWidth="thick";
+    document.getElementById("img3").style.borderColor="red";
+}
